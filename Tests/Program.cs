84a95738e0126b290c673dcca84c3c9adde784dc
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2023-12-18 22:45:37 +0900

    more docfx tests

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,7 +1,12 @@
+using System;
+
 namespace SatorImaging.DocFXPages.Tests
 {
+    /// <summary>Hi, `shown as inline code?`</summary>
     public class TestClass
     {
+        /// <summary>Summary</summary>
+        /// <remarks>Remarks</remarks>
         public static string StaticStringField;
         public float FloatField;
 
@@ -10,12 +15,30 @@ namespace SatorImaging.DocFXPages.Tests
 
     public sealed class SealedClass : TestClass
     {
+        [Obsolete]
+        public static int StaticObsoleteField;
+
+        [Obsolete("Obsolete!")]
+        public double ObsoleteField;
+
         public SealedClass() { }
         public SealedClass(float val) { }
     }
 
+    [Obsolete("This class is deprecated!")]
+    public class DeprecatedClass {}
+
+    /// <summary>
+    /// This is sealed class.<br/>
+    /// markdown enabled?? <a href='https://www.sator-imaging.com/'>HTML Link Tag</a>
+    /// ```csharp
+    /// // code block.
+    /// UnityEngine.Debug.Log("Hello, world.");
+    /// ```
+    /// </summary>
     public static class Program
     {
+        /// <summary>**Is this shown as bold??**</summary>
         public static void Main(string[] args) { }
     }
 }
