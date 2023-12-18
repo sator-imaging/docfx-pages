8881f3c35eb788de73607168cdc8bf647ef8a6a9
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2023-12-19 00:45:40 +0900

    more docfx style test

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,4 +1,5 @@
 using System;
+usng System.ComponentModel;
 
 namespace SatorImaging.DocFXPages.Tests.Instances
 {
@@ -8,11 +9,13 @@ namespace SatorImaging.DocFXPages.Tests.Instances
         /// <summary>Summary</summary>
         /// <remarks>Remarks</remarks>
         public static string StaticStringField;
+        /// <remarks>Remarks</remarks>
         public float FloatField;
 
         public TestClass() { }
     }
 
+    /// <inheritdoc/>
     public sealed class SealedClass : TestClass
     {
         [Obsolete]
@@ -21,27 +24,49 @@ namespace SatorImaging.DocFXPages.Tests.Instances
         [Obsolete("Obsolete!")]
         public double ObsoleteField;
 
+        [Obsolete("<b>HTML bold tag can be used??</b> **Markdown bold syntax enabled??**")]
         public SealedClass() { }
         public SealedClass(float val) { }
     }
 
+    /// <summary>*Is this shown as italic??*</summary>
     [Obsolete("This class is deprecated!")]
     public class DeprecatedClass {}
 }
 
+namespace SatorImaging.DocFXPages.Tests.Hidden
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class HiddenClass
+    {
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static void HiddenMethod() { }
+    }
+}
+
 namespace SatorImaging.DocFXPages.Tests
 {
+    /// <summary>~~Is this shown as strikethrough??~~</summary>
+    public enum Enum
+    {
+        Default,
+        Exceeded = 9999,
+    }
+
     /// <summary>
     /// This is sealed class.<br/>
-    /// markdown enabled?? <a href='https://www.sator-imaging.com/'>HTML Link Tag</a>
     /// ```csharp
     /// // code block.
     /// UnityEngine.Debug.Log("Hello, world.");
     /// ```
+    /// markdown enabled?? <a href='https://www.sator-imaging.com/'>This is HTML Link Tag</a>
     /// </summary>
     public static class Program
     {
         /// <summary>**Is this shown as bold??**</summary>
+        /// <remarks>none</remarks>
+        /// <returns>void</returns>
+        /// <param name="args">command line args</param>
         public static void Main(string[] args) { }
     }
 }
