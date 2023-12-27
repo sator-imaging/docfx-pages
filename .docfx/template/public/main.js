4404ec73297d1a1c4c23b3ac15c88b48721079cd
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2023-12-27 22:00:12 +0900

    delay for breadcrumb

--- .docfx/template/public/main.js
+++ .docfx/template/public/main.js
@@ -150,20 +150,21 @@ function initializePage(event) {
                     dd.appendChild(div);
                 }
             }
+
+            // breadcrumb is dynamically created. delay required.
+            document.body.dataset.yamlMime = 'ApiPage';
+            const breadcrumb = document.querySelector('nav .breadcrumb');
+            if (breadcrumb) {
+                const li = document.createElement('li');
+                const anchor = document.createElement('a');
+                anchor.innerText = 'API';
+                anchor.href = 'index.html';
+                li.appendChild(anchor);
+                breadcrumb.appendChild(li);
+            }
         }
 
         tocRequest.send();
-
-        document.body.dataset.yamlMime = 'ApiPage';
-        const breadcrumb = document.querySelector('nav .breadcrumb');
-        if (breadcrumb) {
-            const li = document.createElement('li');
-            const anchor = document.createElement('a');
-            anchor.innerText = 'API';
-            anchor.href = 'index.html';
-            li.appendChild(anchor);
-            breadcrumb.appendChild(li);
-        }
     }
 
 }
