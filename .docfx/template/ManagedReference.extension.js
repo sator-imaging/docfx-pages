d2f06f2bcc74a9979ad7873a3fd0fc7d686bde72
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2024-01-12 20:03:08 +0900

    support nested data

--- .docfx/template/ManagedReference.extension.js
+++ .docfx/template/ManagedReference.extension.js
@@ -13,17 +13,29 @@ exports.preTransform = function (model) {
  */
 exports.postTransform = function (model) {
 
-    // fix source code link. see define_symbols.txt for detail
-    if (model.sourceurl) {
-        let link = new String(model.sourceurl);
-
-        var p = link.search(/[0-9]+$/);
-        if (p >= 0) {
-            model.sourceurl
-                = link.substring(0, p)
-                + (parseInt(link.substring(p)) - 112);  // <-- subtract define_symbols.txt line count
+    fixSourceCodeAnchor(model.sourceurl);
+    for (const child of model.children) {
+        fixSourceCodeAnchor(child.sourceurl);
+        for (const grand of child.children) {
+            fixSourceCodeAnchor(grand.sourceurl);
         }
     }
 
     return model;
+}
+
+
+function fixSourceCodeAnchor(modelData) {
+    // fix source code link. see define_symbols.txt for detail
+    if (!modelData || !modelData.sourceurl)
+        return;
+
+    let link = new String(modelData.sourceurl);
+
+    var p = link.search(/[0-9]+$/);
+    if (p >= 0) {
+        modelData.sourceurl
+            = link.substring(0, p)
+            + (parseInt(link.substring(p)) - 112);  // <-- subtract define_symbols.txt line count
+    }
 }
