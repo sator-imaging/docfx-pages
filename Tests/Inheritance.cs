b6bfed87eab6e448786858e4dbc7dd0251f525bf
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2024-02-20 19:44:35 +0900

    tests: namespace!!

--- Tests/Inheritance.cs
+++ Tests/Inheritance.cs
@@ -1,28 +1,31 @@
 using System;
 
-public interface ITest
+namespace DocFXPages.Tests.DocInheritance
 {
-    /// <summary>ITest.DoItInterface</summary>
-    void DoItInterface();
-}
+    public interface ITest
+    {
+        /// <summary>ITest.DoItInterface</summary>
+        void DoItInterface();
+    }
 
-public abstract class Base : ITest
-{
-    public void DoIt() { }
-    public void DoItInterface() { }
+    public abstract class Base : ITest
+    {
+        public void DoIt() { }
+        public void DoItInterface() { }
 
-    /// <summary>Base.DoItVirtual</summary>
-    public virtual void DoItVirtual() { }
+        /// <summary>Base.DoItVirtual</summary>
+        public virtual void DoItVirtual() { }
 
-    /// <summary>Base.DoItAbstract</summary>
-    public abstract void DoItAbstract();
+        /// <summary>Base.DoItAbstract</summary>
+        public abstract void DoItAbstract();
 
-    /// <summary>Base.DoItVirtualOverridden</summary>
-    public virtual void DoItVirtualOverridden() { }
-}
+        /// <summary>Base.DoItVirtualOverridden</summary>
+        public virtual void DoItVirtualOverridden() { }
+    }
 
-public class Concrete : Base
-{
-    public override void DoItAbstract() { }
-    public override void DoItVirtualOverridden() { }
+    public class Concrete : Base
+    {
+        public override void DoItAbstract() { }
+        public override void DoItVirtualOverridden() { }
+    }
 }
