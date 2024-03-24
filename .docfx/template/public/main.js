00b0230457c4777983b1dd458acd96ed4ff5ab76
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2024-03-24 14:48:48 +0900

    danger -> warning

--- .docfx/template/public/main.js
+++ .docfx/template/public/main.js
@@ -1,32 +1,3 @@
-/*
-function initializeAffix() {
-    const affixContainer = document.querySelector('div.affix');
-    if (!affixContainer || (affixContainer.offsetWidth == 0 && affixContainer.offsetHeight == 0))
-        return;
-
-    // badge for affix
-    for (const affix of document.querySelectorAll('body[data-yaml-mime=ApiPage] div.affix a')) {
-        if (affix.innerText.endsWith(' Deprecated')) {
-            affix.innerText = affix.innerText.replace(/ Deprecated$/, '') + ' ';
-
-            let badge = document.createElement('span');
-            badge.innerText = '✖️';
-            badge.classList.add("badge", "rounded-pill", "text-bg-danger");
-            affix.appendChild(badge);
-        }
-    }
-
-    window.removeEventListener("resize", initializeAffix);
-}
-
-window.addEventListener("resize", initializeAffix);
-// need delay for affix async load
-// run 3 different delays, no way to await unknown async jobs. this works enough.
-setTimeout(initializeAffix, 64);
-setTimeout(initializeAffix, 128);
-setTimeout(initializeAffix, 1024);
-*/
-
 function initializePage(event) {
     // TODO: do this by customizing template/partials.
     const stylingByScript = function () {
